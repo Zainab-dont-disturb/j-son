0ac858e7c888bec68edc2473a39f6bfb402b42c3
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-11-18 17:14:16 +0000

    DONE

---
+++ script.js
@@ -0,0 +1,4 @@
+var Informations={"name":"Zainab","age":13,"hobby":"code"}
+document.getElementById("h1").innerHTML= Informations;
+var c=JSON.stringify(Informations);
+document.getElementById("h12").innerHTML= c;
